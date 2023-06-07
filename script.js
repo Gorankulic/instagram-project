23df1c5cb00ddace5b99cc5a9c1c33555f5db629
Goran <gorankulic@outlook.comm>
2023-06-08 01:46:44 +0200

    added Render the posts vertically from top to bottom in the DOM.s

--- script.js
+++ script.js
@@ -34,6 +34,53 @@ function renderStories() {
     // Add the stories container to the body of the page.
     document.body.appendChild(storiesContainer);
 }
+/**
+ * List of posts with images and names.
+ * @type {Array}
+ */
+const posts = [{
+        src: '/img/posts/post (1).jpg',
+        name: 'Name 1'
+    },
+    {
+        src: '/img/posts/post (2).jpg',
+        name: 'Name 2'
+    },
+    {
+        src: '/img/posts/post (3).jpg',
+        name: 'Name 3'
+    },
+    {
+        src: '/img/posts/post (4).jpg',
+        name: 'Name 4'
+    }
+];
 
-// Render the stories when the page loads.
-window.onload = renderStories;
+/**
+ * Renders the posts vertically from top to bottom in the DOM.
+ */
+function renderPosts() {
+    const container = document.getElementById('posts');
+    container.innerHTML = '';
+
+    posts.forEach(post => {
+        const element = document.createElement('div');
+        element.classList.add('post');
+
+        const image = new Image();
+        image.src = post.src;
+        image.alt = post.name;
+        image.width = '200';
+        image.height = '200';
+
+        const name = document.createElement('h3');
+        name.textContent = post.name;
+
+        element.appendChild(image);
+        element.appendChild(name);
+
+        container.appendChild(element);
+    });
+}
+renderStories();
+renderPosts();

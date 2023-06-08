7d503f20ce9a87f5a936a4b7fe2e2cabfe780c3d
Goran <gorankulic@outlook.comm>
2023-06-08 02:20:46 +0200

    added comment section, needs editing

--- script.js
+++ script.js
@@ -5,13 +5,8 @@ function renderStories() {
     const storiesContainer = document.createElement('div');
     storiesContainer.classList.add('stories');
 
-    // Definiere die Liste der Bildquellen und entsprechenden Namen.
-    const images = [
-        { src: '/img/people profile pictures/pic (2).jpg', name: 'Name 2' },
-        { src: '/img/people profile pictures/pic (1).jpg', name: 'Name 1' },
-        { src: '/img/people profile pictures/pic (3).jpg', name: 'Name 3' },
-        { src: '/img/people profile pictures/pic (4).jpg', name: 'Name 4' },
-    ];
+    // Definiere die Liste der Bildquellen und generiere zufällige Namen.
+    const images = generateRandomImages();
 
     // Iteriere durch die Liste der Bilder und erstelle die Bild- und Namen-Elemente.
     for (const image of images) {
@@ -39,31 +34,7 @@ function renderStories() {
  * Liste von Beiträgen mit Bildern, Namen und Kommentaren.
  * @type {Array}
  */
-let posts = [{
-        src: '/img/posts/post (1).jpg',
-        name: 'Name 1',
-        likes: 0,
-        comments: [],
-    },
-    {
-        src: '/img/posts/post (2).jpg',
-        name: 'Name 2',
-        likes: 0,
-        comments: [],
-    },
-    {
-        src: '/img/posts/post (3).jpg',
-        name: 'Name 3',
-        likes: 0,
-        comments: [],
-    },
-    {
-        src: '/img/posts/post (4).jpg',
-        name: 'Name 4',
-        likes: 0,
-        comments: [],
-    },
-];
+let posts = generateRandomPosts();
 
 /**
  * Rendert die Beiträge vertikal von oben nach unten im DOM.
@@ -73,7 +44,7 @@ function renderPosts() {
     container.innerHTML = '';
 
     posts.forEach((post, index) => {
-        const element = createPostElement(post);
+        const element = createPostElement(post, index);
 
         container.appendChild(element);
     });
@@ -82,9 +53,10 @@ function renderPosts() {
 /**
  * Erstellt das Element für einen einzelnen Beitrag.
  * @param {Object} post - Der Beitrag.
+ * @param {number} index - Der Index des Beitrags.
  * @returns {HTMLElement} - Das erstellte Element.
  */
-function createPostElement(post) {
+function createPostElement(post, index) {
     const element = document.createElement('div');
     element.classList.add('post');
 
@@ -101,9 +73,9 @@ function createPostElement(post) {
     likeCount.textContent = `Likes: ${post.likes}`;
     likeCount.classList.add('like-count');
 
-    const commentSection = createCommentSection(post.comments);
+    const commentSection = createCommentSection(post.comments, index);
 
-    const likeButton = createLikeButton(post);
+    const likeButton = createLikeButton(post, index);
 
     element.appendChild(image);
     element.appendChild(name);
@@ -117,24 +89,24 @@ function createPostElement(post) {
 /**
  * Erstellt den Kommentarbereich für einen Beitrag.
  * @param {Array} comments - Die Kommentare des Beitrags.
+ * @param {number} postIndex - Der Index des Beitrags.
  * @returns {HTMLElement} - Das erstellte Element.
  */
-function createCommentSection(comments) {
+function createCommentSection(comments, postIndex) {
     const commentSection = document.createElement('div');
     commentSection.classList.add('comment-section');
 
     const commentsList = document.createElement('ul');
     commentsList.classList.add('comments-list');
 
-    comments.forEach((comment) => {
-        const commentItem = document.createElement('li');
-        commentItem.textContent = comment;
+    comments.forEach((comment, commentIndex) => {
+        const commentItem = createCommentItem(comment, commentIndex, postIndex);
         commentsList.appendChild(commentItem);
     });
 
     commentSection.appendChild(commentsList);
 
-    const addCommentForm = createCommentForm();
+    const addCommentForm = createCommentForm(postIndex);
 
     commentSection.appendChild(addCommentForm);
 
@@ -143,16 +115,16 @@ function createCommentSection(comments) {
 
 /**
  * Erstellt das Formular zum Hinzufügen eines Kommentars.
+ * @param {number} postIndex - Der Index des Beitrags.
  * @returns {HTMLElement} - Das erstellte Formular.
  */
-function createCommentForm() {
+function createCommentForm(postIndex) {
     const addCommentForm = document.createElement('form');
     addCommentForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const commentInput = addCommentForm.querySelector('input');
         const comment = commentInput.value;
         if (comment.trim() !== '') {
-            const postIndex = Array.from(addCommentForm.closest('.post').parentNode.children).indexOf(addCommentForm.closest('.post'));
             posts[postIndex].comments.push(comment);
             renderPosts();
             savePostsToLocalStorage();
@@ -173,20 +145,136 @@ function createCommentForm() {
 }
 
 /**
- * Erstellt den "Like"-Button für einen Beitrag.
- * @param {Object} post - Der Beitrag.
+ * Erstellt ein einzelnes Kommentar-Element.
+ * @param {string} comment - Der Kommentarinhalt.
+ * @param {number} commentIndex - Der Index des Kommentars.
+ * @param {number} postIndex - Der Index des Beitrags.
  * @returns {HTMLElement} - Das erstellte Element.
  */
-function createLikeButton(post) {
+function createCommentItem(comment, commentIndex, postIndex) {
+    const commentItem = document.createElement('li');
+    commentItem.textContent = comment;
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Bearbeiten';
+    editButton.addEventListener('click', () => {
+        editComment(commentIndex, postIndex, commentItem);
+    });
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Löschen';
+    deleteButton.addEventListener('click', () => {
+        deleteComment(commentIndex, postIndex, commentItem);
+    });
+
+    commentItem.appendChild(editButton);
+    commentItem.appendChild(deleteButton);
+
+    return commentItem;
+}
+
+/**
+ * Bearbeitet einen Kommentar.
+ * @param {number} commentIndex - Der Index des Kommentars.
+ * @param {number} postIndex - Der Index des Beitrags.
+ * @param {HTMLElement} commentItem - Das Kommentar-Element.
+ */
+function editComment(commentIndex, postIndex, commentItem) {
+    const commentText = posts[postIndex].comments[commentIndex];
+    const editedText = prompt('Kommentar bearbeiten:', commentText);
+
+    if (editedText !== null) {
+        posts[postIndex].comments[commentIndex] = editedText;
+        commentItem.textContent = editedText;
+        savePostsToLocalStorage();
+    }
+}
+
+/**
+ * Löscht einen Kommentar.
+ * @param {number} commentIndex - Der Index des Kommentars.
+ * @param {number} postIndex - Der Index des Beitrags.
+ * @param {HTMLElement} commentItem - Das Kommentar-Element.
+ */
+function deleteComment(commentIndex, postIndex, commentItem) {
+    posts[postIndex].comments.splice(commentIndex, 1);
+    commentItem.remove();
+    savePostsToLocalStorage();
+}
+
+/**
+ * Erstellt den "Like"-Button für einen Beitrag.
+ * @param {Object} post - Der Beitrag.
+ * @param {number} postIndex - Der Index des Beitrags.
+ * @returns {HTMLElement} - Das erstellte Element.
+ */
+function createLikeButton(post, postIndex) {
     const likeButton = document.createElement('button');
     likeButton.innerHTML = `<i class="fas fa-heart"></i> Like (${post.likes})`;
     likeButton.addEventListener('click', () => {
-        post.likes++;
+        if (likeButton.classList.contains('liked')) {
+            post.likes--;
+            likeButton.classList.remove('liked');
+        } else {
+            post.likes++;
+            likeButton.classList.add('liked');
+        }
         renderPosts();
         savePostsToLocalStorage();
     });
 
+    // Überprüfen, ob der Beitrag bereits geliked wurde
+    if (post.likes > 0) {
+        likeButton.classList.add('liked');
+    }
+
     return likeButton;
+}
+
+/**
+ * Generiert einen zufälligen Namen.
+ * @returns {string} - Der generierte Name.
+ */
+function generateRandomName() {
+    const names = ['Max', 'Lena', 'Tim', 'Anna', 'Felix', 'Sarah', 'Paul', 'Laura', 'David', 'Julia'];
+    return names[Math.floor(Math.random() * names.length)];
+}
+
+/**
+ * Generiert eine Liste von zufälligen Bildern mit zugehörigen Namen.
+ * @returns {Array} - Die generierten Bilder.
+ */
+function generateRandomImages() {
+    const images = [
+        { src: '/img/people profile pictures/pic (2).jpg', name: generateRandomName() },
+        { src: '/img/people profile pictures/pic (1).jpg', name: generateRandomName() },
+        { src: '/img/people profile pictures/pic (3).jpg', name: generateRandomName() },
+        { src: '/img/people profile pictures/pic (4).jpg', name: generateRandomName() },
+    ];
+
+    return images;
+}
+
+/**
+ * Generiert eine Liste von zufälligen Beiträgen.
+ * @returns {Array} - Die generierten Beiträge.
+ */
+function generateRandomPosts() {
+    const posts = [];
+
+    const images = generateRandomImages();
+
+    images.forEach((image) => {
+        const post = {
+            src: image.src,
+            name: image.name,
+            likes: 0,
+            comments: [],
+        };
+        posts.push(post);
+    });
+
+    return posts;
 }
 
 /**

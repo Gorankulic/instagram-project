9c29955668bc005ad6dc6751efa65eb2ef31ba05
Goran <gorankulic@outlook.comm>
2023-06-10 00:53:10 +0200

    added json array with values

---
+++ script.js
@@ -0,0 +1,26 @@
+[{
+        "name": "Person 1",
+        "comments": ["Great photo!", "Love it!", "So beautiful!"],
+        "likes": 10
+    },
+    {
+        "name": "Person 2",
+        "comments": ["Amazing!", "Awesome shot!", "You're killing it!"],
+        "likes": 5
+    },
+    {
+        "name": "Person 3",
+        "comments": ["Fantastic!", "Incredible work!", "You never disappoint!"],
+        "likes": 8
+    },
+    {
+        "name": "Person 4",
+        "comments": ["This is stunning!", "Absolutely love it!", "You're a talent!"],
+        "likes": 3
+    },
+    {
+        "name": "Person 5",
+        "comments": ["So joyful!", "Bringing smiles!", "You brighten my day!"],
+        "likes": 12
+    }
+]

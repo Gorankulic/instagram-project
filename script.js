b15f4a3acea13acdefdb13c364b35ae647fa5996
Goran <gorankulic@outlook.comm>
2023-06-12 16:42:55 +0200

    updated json array settings

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 [{
         "name": "Person 1",
         "comments": ["Great photo!", "Love it!", "So beautiful!"],
-        "likes": 10
+        "likes": 10,
+        "picture-location": "img/posts/post (1).jpg"
     },
     {
         "name": "Person 2",
         "comments": ["Amazing!", "Awesome shot!", "You're killing it!"],
-        "likes": 5
+        "likes": 5,
+        "picture-location": "img/posts/post (2).jpg"
     },
     {
         "name": "Person 3",
         "comments": ["Fantastic!", "Incredible work!", "You never disappoint!"],
-        "likes": 8
+        "likes": 8,
+        "picture-location": "img/posts/post (3).jpg"
     },
     {
         "name": "Person 4",
         "comments": ["This is stunning!", "Absolutely love it!", "You're a talent!"],
-        "likes": 3
-    },
-    {
-        "name": "Person 5",
-        "comments": ["So joyful!", "Bringing smiles!", "You brighten my day!"],
-        "likes": 12
+        "likes": 3,
+        "picture-location": "img/posts/post (4).jpg"
     }
 ]

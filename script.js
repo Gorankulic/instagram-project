9ccfa4c0285e0d9994aa3faac492e967b2c80241
Goran <gorankulic@outlook.comm>
2023-06-13 01:31:52 +0200

    json array is now updated and it shows all buttons

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const usersData = [{
-            "name": "Goran",
-            "comments": ["Great photo!", "Love it!", "So beautiful!"],
-            "likes": 10,
-            "picture-location": "img/posts/post (1).jpg",
-            "profile-picture": "img/people profile pictures/pic (1).jpg",
-            "status": "Enjoying life to the fullest!"
-        },
-        {
-            "name": "Rob",
-            "comments": ["Amazing!", "Awesome shot!", "You're killing it!"],
-            "likes": 5,
-            "picture-location": "img/posts/post (2).jpg",
-            "profile-picture": "img/people profile pictures/pic (2).jpg",
-            "status": "Chasing dreams and making memories!"
-        },
-        {
-            "name": "Tom und Anna",
-            "comments": ["Fantastic!", "Incredible work!", "You never disappoint!"],
-            "likes": 8,
-            "picture-location": "img/posts/post (3).jpg",
-            "profile-picture": "img/people profile pictures/pic (3).jpg",
-            "status": "Embracing the beauty of nature!"
-        },
-        {
-            "name": "Birgit",
-            "comments": ["This is stunning!", "Absolutely love it!", "You're a talent!"],
-            "likes": 3,
-            "picture-location": "img/posts/post (4).jpg",
-            "profile-picture": "img/people profile pictures/pic (4).jpg",
-            "status": "Exploring new horizons!"
-        }
-    ]
-
-    /**
-     * Creates an HTML element with the given tag name, attributes, and inner text.
-     * @param {string} tagName - The name of the tag for the HTML element.
-     * @param {Object} attributes - An object mapping attribute names to attribute values.
-     * @param {string} [innerText] - Optional. The inner text of the HTML element.
-     * @return {HTMLElement} The created HTML element.
-     */
+                "name": "Goran",
+                "comments": ["Great photo!", "Love it!", "So beautiful!"],
+                "likes": 10,
+                "picture-location": "img/posts/post (1).jpg",
+                "profile-picture": "img/people profile pictures/pic (1).jpg",
+                "status": "Enjoying life to the fullest!"
+            },
+            {
+                "name": "Rob",
+                "comments": ["Amazing!", "Awesome shot!", "You're killing it!"],
+                "likes": 5,
+                "picture-location": "img/posts/post (2).jpg",
+                "profile-picture": "img/people profile pictures/pic (2).jpg",
+                "status": "Chasing dreams and making memories!"
+            },
+            {
+                "name": "Tom und Anna",
+                "comments": ["Fantastic!", "Incredible work!", "You never disappoint!"],
+                "likes": 8,
+                "picture-location": "img/posts/post (3).jpg",
+                "profile-picture": "img/people profile pictures/pic (3).jpg",
+                "status": "Embracing the beauty of nature!"
+            },
+            {
+                "name": "Birgit",
+                "comments": ["This is stunning!", "Absolutely love it!", "You're a talent!"],
+                "likes": 3,
+                "picture-location": "img/posts/post (4).jpg",
+                "profile-picture": "img/people profile pictures/pic (4).jpg",
+                "status": "Exploring new horizons!"
+            }
+        ]
+        /**
+         * Creates an HTML element with the given tag name, attributes, and inner text.
+         * @param {string} tagName - The name of the tag for the HTML element.
+         * @param {Object} attributes - An object mapping attribute names to attribute values.
+         * @param {string} [innerText] - Optional. The inner text of the HTML element.
+         * @return {HTMLElement} The created HTML element.
+         */
     function createElement(tagName, attributes, innerText) {
         const element = document.createElement(tagName);
 
@@ -91,9 +90,53 @@ document.addEventListener('DOMContentLoaded', (event) => {
             container.appendChild(postContainerDiv);
 
             // Buttons-container
-            const buttonsContainerDiv = createElement('div', { class: 'buttons-container' });
+            const buttonsContainerDiv = createElement('div', {
+                class: 'buttons-container',
+                id: 'icons-container-parents'
+            });
 
-            // ... Create and append the inner elements like icons-container, favorite-container
+            // Icons-container
+            const iconsContainerDiv = createElement('div', {
+                class: 'icons-container',
+                id: 'icons-container-kids'
+            });
+
+            // Like button
+            const likeButtonImg = createElement('img', {
+                src: 'img/icons/herz.png',
+                id: 'like-button',
+                alt: 'Like Button'
+            });
+            iconsContainerDiv.appendChild(likeButtonImg);
+
+            // Comment section
+            const commentSectionImg = createElement('img', {
+                src: 'img/icons/comment icon.png',
+                id: 'comment-section',
+                alt: 'Comment Section'
+            });
+            iconsContainerDiv.appendChild(commentSectionImg);
+
+            // Arrow button
+            const arrowButtonImg = createElement('img', {
+                src: 'img/icons/arrow.png',
+                id: 'send-to-friends-arrow',
+                class: 'arrow-button',
+                alt: 'Arrow Button'
+            });
+            iconsContainerDiv.appendChild(arrowButtonImg);
+
+            buttonsContainerDiv.appendChild(iconsContainerDiv);
+
+            // Favorite container
+            const favoriteContainerDiv = createElement('div', { class: 'favorite-container' });
+            const favoriteIconImg = createElement('img', {
+                src: 'img/icons/favorite icon blank.png',
+                id: 'add-to-favorite',
+                alt: 'Favorite Icon'
+            });
+            favoriteContainerDiv.appendChild(favoriteIconImg);
+            buttonsContainerDiv.appendChild(favoriteContainerDiv);
 
             container.appendChild(buttonsContainerDiv);
 
@@ -101,16 +144,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
             const likesDiv = createElement('div', { class: 'likes' });
             const likesP = createElement('p');
             likesP.innerHTML = `Liked by <b>Username</b> and <b id="names-of-the-other-users-who-liked-the-post">others</b>`;
-
             likesDiv.appendChild(likesP);
             container.appendChild(likesDiv);
 
             // Status
             const statusDiv = createElement('div', { class: 'status' });
             const statusUsernameP = createElement('p');
-            statusUsernameP.innerHTML = `<b>${user.name}</b>`;
+            statusUsernameP.innerHTML = `<b>Username</b>`;
             const statusContentP = createElement('p', { id: 'user-status' }, user.status);
-
             statusDiv.appendChild(statusUsernameP);
             statusDiv.appendChild(statusContentP);
             container.appendChild(statusDiv);
@@ -118,7 +159,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
             // View all comments
             const commentsDiv = createElement('div');
             const commentsP = createElement('p', {}, 'View all comments');
-
             commentsDiv.appendChild(commentsP);
             container.appendChild(commentsDiv);
 
@@ -128,7 +168,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 name: 'comment',
                 placeholder: 'Add a comment...'
             });
-
             container.appendChild(commentInput);
         }
     }
